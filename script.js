43aa53f24f5167e3f2324485fed4fdc51d2a6541
Diogo Fiuza <difipalm@gmail.com>
2021-08-12 20:36:41 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+// Cria um array com os products
+function productAll(element) {
+  return element.map((elem) => ({ 
+    sku: elem.id, 
+    name: elem.title, 
+    image: elem.thumbnail,
+   }));
+}
+
+// Adiciona os produtos na página
+function addElement(add) {
+  const items = document.querySelector('.items');
+  items.appendChild(add);
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,6 +29,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Cria os produtos
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -24,20 +42,31 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+function products(api) {
+  return fetch(api)
+    .then((obj) => obj.json())
+    .then((json) => json.results)
+    .then((newPr) => productAll(newPr))
+    .then((product) => product.forEach((obj) => 
+    addElement(createProductItemElement(obj))));
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = () => { };
+window.onload = () => {
+  products(URL);
+ };
